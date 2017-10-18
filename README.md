052b90fd050ad2f4ebde081788b9e670339b13e3
Jeff Modzel <Jeff.Modzel@ellucian.com>
2017-10-18 09:09:46 -0400

    Add node_module and source map support to webpack

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BabelTemplate
 Starter template for ES6/ES2015 with Babel and Webpack
+
+
+# Resources
+http://jlongster.com/Backend-Apps-with-Webpack--Part-I
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 var path = require('path');
 var webpack = require('webpack');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
 
 module.exports = {
     entry: './index.js',
+    target: 'node',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.bundle.js'
@@ -18,6 +29,10 @@ module.exports = {
             }
         ]
     },
+    externals: nodeModules,
+    plugins: [
+        new webpack.BannerPlugin('require("source-map-support").install({environment: "node" });')
+    ],
     stats: {
         colors: true
     },
