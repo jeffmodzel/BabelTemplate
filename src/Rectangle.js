27a04455103eea29fe463ecc155f98ddbd6aec7f
Jeff Modzel <jeff.modzel@ellucian.com>
2018-03-15 21:46:32 -0400

    Add babel class properties

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -1,20 +1,26 @@
 class Rectangle {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
-  // Getter
-  get area() {
-    return this.calcArea();
-  }
-  // Method
-  calcArea() {
-    return this.height * this.width;
-  }
 
-  toString() {
-      return "Rectangle - area: " + this.calcArea();
-  }
+    height = 0;
+    width = 0;
+
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    // Getter
+    get area() {
+        return this.calcArea();
+    }
+
+    // Method
+    calcArea() {
+        return this.height * this.width;
+    }
+
+    toString() {
+        return "Rectangle - area: " + this.calcArea();
+    }
 }
 
 export default Rectangle;
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015']
+                    presets: ['env'],
+                    plugins: ["transform-class-properties"]
                 }
             }
         ]
